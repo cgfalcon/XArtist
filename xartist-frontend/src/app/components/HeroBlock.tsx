332a33047258585e459b2f44e4e81c8a24572b94
chugang.cg <cggogogo@126.com>
2024-07-12 13:56:56 -0400

    Update config

--- xartist-frontend/src/app/components/HeroBlock.tsx
+++ xartist-frontend/src/app/components/HeroBlock.tsx
@@ -7,15 +7,16 @@ function HeroBlock() {
     const tmp_images = [
         // '/wikiart_resnet_60.gif',
         // '/wikiart_resnet_150.gif',
-        '/wikiart_sndcgan_expressionism_300.gif',
-        '/wikiart_sndcgan_stilllife_300.gif',
+        '/wikiart_sndcgan256_bce_impressionism_1_600.gif',
+        '/wikiart_sndcgan256_bce_impressionism_2_600.gif',
+        '/wikiart_sndcgan256_impressionism_1_600.gif',
+        '/wikiart_sndcgan256_impressionism_2_600.gif',
         '/wikiart_sngan_genra_150.gif',
         '/wikiart_frelu_impressionism_050.gif',
         '/wikiart_frelu_impressionism_100.gif',
         '/wikiart_impressionist_animation100.gif',
         '/wikiart_impressionist_animation50.gif',
         '/wikiart_abstract_animation160.gif',
-        '/wikiart_artist_animation.gif',
     ]; // Replace paths with your image paths or URLs
 
     const [index, setIndex] = useState(0);
